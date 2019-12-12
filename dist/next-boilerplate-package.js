ceae6fc6e57e9b9291b55c00946b7bf725143937
afeiship <1290657123@qq.com>
2019-12-12 21:04:03 +0800

    add: new pkg version

---
+++ dist/next-boilerplate-package.js
@@ -0,0 +1,25 @@
+/*!
+ * name: @feizheng/next-boilerplate-package
+ * url: https://github.com/afeiship/next-boilerplate-package
+ * version: 1.0.0
+ * date: 2019-12-12T13:03:58.578Z
+ * license: MIT
+ */
+
+(function () {
+
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('@feizheng/next-js-core2');
+
+  nx.boilerplatePackage = function () {
+    //code goes here.
+  };
+
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.boilerplatePackage;
+  }
+
+}());
+
+//# sourceMappingURL=next-boilerplate-package.js.map
