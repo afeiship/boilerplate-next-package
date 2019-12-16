ad80854107c24816b302acc883c4f21f464832f1
afeiship <1290657123@qq.com>
2019-12-16 23:53:13 +0800

    add: pkg-header

--- dist/next-boilerplate-package.js
+++ dist/next-boilerplate-package.js
@@ -1,25 +1,23 @@
 /*!
  * name: @feizheng/next-boilerplate-package
+ * description: next-boilerplate-package-description
  * url: https://github.com/afeiship/next-boilerplate-package
  * version: 1.0.0
- * date: 2019-12-12T13:03:58.578Z
+ * date: 2019-12-16T15:53:06.987Z
  * license: MIT
  */
 
-(function () {
-
+(function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@feizheng/next-js-core2');
 
-  nx.boilerplatePackage = function () {
+  nx.boilerplatePackage = function() {
     //code goes here.
   };
-
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = nx.boilerplatePackage;
   }
-
-}());
+})();
 
 //# sourceMappingURL=next-boilerplate-package.js.map
--- src/next-boilerplate-package.js
+++ src/next-boilerplate-package.js
@@ -1,15 +1,12 @@
-(function () {
-
+(function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@feizheng/next-js-core2');
 
-  nx.boilerplatePackage = function () {
+  nx.boilerplatePackage = function() {
     //code goes here.
   };
-
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = nx.boilerplatePackage;
   }
-
-}());
+})();
