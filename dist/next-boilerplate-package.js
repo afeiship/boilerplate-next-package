14ee0012768d613825df5501151a2f4275fd6e26
afeiship <1290657123@qq.com>
2020-04-15 14:51:31 +0800

    add: fixed uglify js

--- dist/next-boilerplate-package.js
+++ dist/next-boilerplate-package.js
@@ -3,7 +3,7 @@
  * description: next-boilerplate-package-description
  * url: https://github.com/afeiship/next-boilerplate-package
  * version: 1.0.0
- * date: 2020-03-28 12:59:29
+ * date: 2020-04-15 14:51:22
  * license: MIT
  */
 
