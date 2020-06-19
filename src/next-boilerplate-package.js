4812bfee26b595507dac01dd7fa6ad7d3b3aee70
afeiship <1290657123@qq.com>
2020-06-19 22:41:13 +0800

    feat: remove dist + add default_options

--- src/next-boilerplate-package.js
+++ src/next-boilerplate-package.js
@@ -1,9 +1,11 @@
-(function() {
+(function () {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@feizheng/next-js-core2');
+  var DEFAULT_OPTIONS = { context: global };
 
-  nx.boilerplatePackage = function() {
-    //code goes here.
+  nx.boilerplatePackage = function (inOptions) {
+    var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
+    // package codes...
   };
 
   if (typeof module !== 'undefined' && module.exports) {
